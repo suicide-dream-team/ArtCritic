fe754d8617d86da7fef3eacebe986abc5f5f3d52
just-an-owl <55064514+just-an-owl@users.noreply.github.com>
2021-03-15 18:31:57 +0400

    дополнение к предыдущему

--- ArtCritic Desctop/ArtCritic Desctop/core/QuestionKeeper.cs
+++ ArtCritic Desctop/ArtCritic Desctop/core/QuestionKeeper.cs
@@ -4,15 +4,16 @@ using System.Text;
 
 namespace ArtCritic_Desctop
 {
+    [Serializable]
     class QuestionKeeper//пока что не определили общий вид запросов пусть объект вопроса в игре выглядит так
     {
         public enum State { None, Text, Image, Video, Music, Eror };
-        protected State state = State.None;
-        protected string Text;
-        protected string ImageLink;
-        protected string VideoLink;
-        protected string MusicLink;
-        protected string[] Answers;
+        public State state = State.None;
+        public string Text { get; set; }
+        public string ImageLink { get; set; }
+        public string VideoLink { get; set; } 
+        public string MusicLink { get; set; }
+        public string[] Answers { get; set; }
 
         public QuestionKeeper() { }
 
