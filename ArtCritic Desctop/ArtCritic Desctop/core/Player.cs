ddbb929fe039f234c0997a6ec3fa6efe11340dcf
just-an-owl <55064514+just-an-owl@users.noreply.github.com>
2021-04-05 00:54:04 +0300

    добавил имя

--- ArtCritic Desctop/ArtCritic Desctop/core/Player.cs
+++ ArtCritic Desctop/ArtCritic Desctop/core/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +7,7 @@ namespace ArtCritic_Desctop
     public class Player//вообще временный только для демки
     {
         public int statistic { get; set; }
+        public string name { get; set; }
         public Player() { }
     }
 }
