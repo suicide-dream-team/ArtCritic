17f3fce7135f981f8925096832d7ed2bb193c33e
just-an-owl <55064514+just-an-owl@users.noreply.github.com>
2021-04-05 00:55:06 +0300

    расширил до рабочего состояния

--- ArtCritic Desctop/ArtCritic Desctop/core/Music_question.cs
+++ ArtCritic Desctop/ArtCritic Desctop/core/Music_question.cs
@@ -1,14 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Media;
+
 
 namespace ArtCritic_Desctop
 {
     class Music_question : TextQuestion
     {
-        public Music_question(string text, string[] answers) : base(text, answers)
+        MediaPlayer music;
+        public Music_question(string text, string[] answers, Uri uriQestion) : base(text, answers)
         {
-
+            music = new MediaPlayer();
+            music.Open(uriQestion);
+            music.Stop();
+        }
+        public void Play()
+        {
+            music.Play();
+        }
+        public void Pause()
+        {
+            music.Pause();
+        }
+        public void Stop()
+        {
+            music.Stop();
         }
     }
 }
